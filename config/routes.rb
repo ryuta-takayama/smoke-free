83c1dc61d0957ee9ed3bb65324d7320c5e8e739f
ryuta takayama <tryuta9@gmail.com>
2025-11-03 22:44:28 +0900

    ダッシュボード（メイン）画面のルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
+  
+  resources :users, only: [] do
+    resource :dashbords, only: [:show]
+  end
+
+
 end
