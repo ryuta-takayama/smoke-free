4679819ec209f70a3623c96a51f716c18013e7b3
ryuta takayama <tryuta9@gmail.com>
2025-11-03 22:57:05 +0900

    新規登録・ログイン後のページ遷移設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+ def after_sign_up_path_for(resource)
+  user_dashbords_path(resource)
+ end
 
+ def after_sign_in_path_for(resource)
+  user_dashbords_path(resource)
+ end
 
   private
 
