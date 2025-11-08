3ccb169e5a960788667ce794498011f4606b2120
ryuta takayama <tryuta9@gmail.com>
2025-11-09 01:14:26 +0900

    表示時間の設定追加

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
     @user = current_user
     @calc = SmokingCalculator.new(@user)
     @session_start = @calc.current_session_start
-    @posts = @user.posts.order(created_at: :asc)
+    today_range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    @posts = @user.posts.where(created_at: today_range).order(created_at: :asc)
  end
 end
