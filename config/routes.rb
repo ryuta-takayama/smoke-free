69d052135d8ee7876ec0c0b621a32d76b50f79f5
ryuta takayama <tryuta9@gmail.com>
2025-11-08 18:26:22 +0900

    postsテーブル、ルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
   resource :dashbords, only: [:show]
-
+  resource :posts
+  
 
 end
