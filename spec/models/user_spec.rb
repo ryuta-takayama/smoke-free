17236ca1111cd8fd6e512eedb858e2791fe4c9eb
ryuta takayama <tryuta9@gmail.com>
2025-11-12 02:48:52 +0900

    user単体モデルテスト（バリテーション関連）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do
-    
+ 
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+
+    it '必要な情報が全て存在すれば登録できる' do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'passwordが6文字以上であれば登録できる' do
+      user = FactoryBot.build(:user, password: 'abc123', password_confirmation: 'abc123')
+      expect(user).to be_valid
+    end
+
+    it 'passwordが英字と数字の両方を含んでいれば登録できる' do
+      user = FactoryBot.build(:user, password: 'abc123', password_confirmation: 'abc123')
+      expect(user).to be_valid
+    end
+
+    [
+       'aa',    
+       'ＡＡ',   
+       '11',    
+       '１１',   
+       '太郎',  
+       'ab　',  
+       '　ab',  
+       'a b' 
+    ].each do |valid_nickname|
+      it "#{valid_nickname.inspect}は有効なニックネームである" do
+      user = FactoryBot.build(:user, nickname: valid_nickname)
+      expect(user).to be_valid
+      end
+     end
+
+     it 'ageが20以上であれば登録できる' do
+       user = FactoryBot.build(:user, age: 25)
+       expect(user).to be_valid
+     end
+
+     it 'reason_to_quitが存在すれば登録できる' do
+       user = FactoryBot.build(:user, reason_to_quit: 'health')
+       expect(user).to be_valid
+     end
+     
+     it 'enum reason_to_quitの値が有効であれば登録できる' do
+        valid_reasons = ['health','money','family','work','other']
+        valid_reasons.each do |reason|
+          user = FactoryBot.build(:user, reason_to_quit: reason)
+          expect(user).to be_valid
+        end
+      end
+  end
+ end
 end
  
