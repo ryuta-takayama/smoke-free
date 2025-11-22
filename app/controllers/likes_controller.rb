d8a042e8a14eb7791149df755f4c5193819647c3
ryuta takayama <tryuta9@gmail.com>
2025-11-23 05:51:18 +0900

    いいね機能実装

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,40 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    toggle_like
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: render_like_panel }
+      format.html { redirect_back fallback_location: @post }
+    end
+  end
+
+  def destroy
+    current_user.likes.find_by(post: @post)&.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: render_like_panel }
+      format.html { redirect_back fallback_location: @post }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def toggle_like
+    if (existing = current_user.likes.find_by(post: @post))
+      existing.destroy
+    else
+      current_user.likes.create(post: @post)
+    end
+  end
+
+  def render_like_panel
+    turbo_stream.replace(view_context.dom_id(@post, :like_panel),
+      partial: "posts/like_panel",
+      locals: { post: @post })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :dashboards
   resources :posts do
     resources :comments, only: [:create, :destroy]
-    resources :likes, only: [:create, :destroy]
+    resource :like, only: [:create, :destroy]
   end
 
 
